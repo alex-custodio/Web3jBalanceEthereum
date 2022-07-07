ac4e6c953b0c9cda6057e4488ffc2fe64a71ef6e
alexcstd <alex.scustod@gmail.com>
2022-07-07 04:16:25 -0300

    my files

---
+++ src/main/java/com/mycompany/web3jbalance/Web3jBalance.java
@@ -0,0 +1,34 @@
+package com.mycompany.web3jbalance;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.http.HttpService;
+import java.util.Scanner;
+
+public class Web3jBalance {
+
+    public static void main(String[] args) throws TimeoutException, InterruptedException, ExecutionException {
+        final Web3j client = Web3j.build(
+                new HttpService("insert here your infura.io mainnet endpoint")
+        );
+        Scanner input = new Scanner(System.in);
+        final String ethAdress = input.nextLine(); //insert here your ethereum adress
+        
+        final EthGetBalance balanceResponse = client.ethGetBalance(
+                ethAdress, DefaultBlockParameter.valueOf("latest")).sendAsync().get(10, TimeUnit.SECONDS);
+        final BigInteger unscaledBalance = balanceResponse.getBalance();
+        final BigDecimal scaledBalance = new BigDecimal(unscaledBalance).divide(
+                new BigDecimal(1000000000000000000L),18,RoundingMode.HALF_UP
+        );
+        System.out.println("Unscaled Balance: " + unscaledBalance);
+        System.out.println("Scaled Balance: " + scaledBalance + " Ether");
+        
+    }
+}
